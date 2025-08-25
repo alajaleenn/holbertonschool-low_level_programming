438d9a0c4a0c556842e14439dad2b7a0f26a6be7
root <alajaleenn1@gmail.com>
2025-08-25 17:14:31 +0000

    push

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+
 /**
- * main - Entry Point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -9,3 +10,4 @@ int main(void)
     puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
+
