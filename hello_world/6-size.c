9820fd3019617129bcb2e58afd979ab8e4a283bd
root <alajaleenn1@gmail.com>
2025-08-26 16:28:55 +0000

    add

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
-
+ * main - Entry point
+ *
+ * My function
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 char achar;
@@ -13,10 +13,10 @@ int aint;
 long int alongint;
 long long int alonglongint;
 float afloat;
-printf("Size of a char: %lu byte(s)\n",sizeof(achar));
-printf("Size of an int: %lu byte(s)\n",sizeof(aint));
-printf("Size of a long int: %lu byte(s)\n",sizeof(alongint));
-printf("Size of a long long int: %lu byte(s)\n",sizeof(alonglongint));
-printf("Size of a float: %lu byte(s)\n",sizeof(afloat));
+printf("Size of a char: %lu byte(s)\n", sizeof(achar));
+printf("Size of an int: %lu byte(s)\n", sizeof(aint));
+printf("Size of a long int: %lu byte(s)\n", sizeof(alongint));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglongint));
+printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 return (0);
 }
