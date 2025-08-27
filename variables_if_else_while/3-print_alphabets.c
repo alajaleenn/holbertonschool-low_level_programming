583eaca8b560607579d6e40d5391586950b218c6
Najla Alajaleen <alajaleenn1@gmail.com>
2025-08-27 18:25:43 +0300

    Add content to 3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet
+ *
+ * My function: loop
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    int a;
+
+    for (a = 0; a < 26; a++)
+        putchar(97 + a);
+    for (a = 0; a < 26; a++)
+        putchar(65 + a);
+    putchar('\n');
+    return (0);
+}
