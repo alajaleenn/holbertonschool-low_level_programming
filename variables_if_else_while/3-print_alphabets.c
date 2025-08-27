f9d18874a725279429b960a2b7e40f0d84d1fe09
Najla Alajaleen <alajaleenn1@gmail.com>
2025-08-27 18:26:41 +0300

    Fix Betty style in 3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -3,19 +3,16 @@
 /**
  * main - Print the alphabet
  *
- * My function: loop
- *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-    int a;
+	int a;
 
-    for (a = 0; a < 26; a++)
-        putchar(97 + a);
-    for (a = 0; a < 26; a++)
-        putchar(65 + a);
-    putchar('\n');
-    return (0);
+	for (a = 0; a < 26; a++)
+		putchar(97 + a);
+	for (a = 0; a < 26; a++)
+		putchar(65 + a);
+	putchar('\n');
+	return (0);
 }
