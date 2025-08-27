0b3b6827f615f2c9168a03ce523922b5d20052c3
Najla Alajaleen <alajaleenn1@gmail.com>
2025-08-27 18:29:57 +0300

    Update 3-print_alphabets.c to match 2-print_alphabet.c structure

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -3,16 +3,17 @@
 /**
  * main - Print the alphabet
  *
+ * My function: loop
+ *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int a;
 
 	for (a = 0; a < 26; a++)
 		putchar(97 + a);
-	for (a = 0; a < 26; a++)
-		putchar(65 + a);
 	putchar('\n');
 	return (0);
 }
