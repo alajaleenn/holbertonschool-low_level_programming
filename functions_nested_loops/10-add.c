ab79366379c7b3474a62f84ba81be9bba9362bf3
Najla Alajaleen <alajaleenn1@gmail.com>
2025-08-29 21:07:24 +0300

    Add 10-add.c with add function and make it executable

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @i: integer
+ * @c: integer
+ *
+ * My function
+ *
+ * Return: Always the results (Success)
+ */
+
+int add(int i, int c)
+{
+	return (i + c);
+}
+
+
