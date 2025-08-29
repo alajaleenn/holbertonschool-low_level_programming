5c4d402bda9903c4481218059c60060213c7825a
Najla Alajaleen <alajaleenn1@gmail.com>
2025-08-29 17:56:13 +0300

    Add 2-print_alphabet_x10.c with print_alphabet_x10 function and make it executable

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print alphabet 10 times
+ *
+ * My function
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int a;
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (a = 0; a < 26; a++)
+			_putchar(97 + a);
+		_putchar('\n');
+		i++;
+	}
+}
+
+
