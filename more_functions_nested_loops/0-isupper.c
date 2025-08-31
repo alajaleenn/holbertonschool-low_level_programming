6b7fc75df20b3012681ef8a9f1ca08828acaa404
root <alajaleenn1@gmail.com>
2025-08-31 15:29:40 +0000

    update

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -3,12 +3,10 @@
 /**
 * _isupper - checks if a character is uppercase
 * @c: The character (in ASCII code) to check
-*
 * Description:
 * In ASCII, uppercase letters 'A' to 'Z' are represented
 * by the values 65 to 90. This function compares the
 * integer value of c to that range.
-*
 * Return: 1 if c is uppercase, 0 otherwise
 */
 int _isupper(int c)
