4689c04ab35ab1cedeb3be4be4ca556a97cf76a5
root <alajaleenn1@gmail.com>
2025-09-02 15:55:08 +0000

    push

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ *
+ * @a: int
+ * @b: int
+ *
+ * My function
+ *
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
