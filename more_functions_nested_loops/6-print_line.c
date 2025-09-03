14b20553aa26f78648115e50b54aeb3227203bce
root <alajaleenn1@gmail.com>
2025-09-03 15:30:43 +0000

    push

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -10,19 +10,17 @@
  *
  * Return: Always 0 (Success)
  */
-
 void print_line(int n)
 {
-	int i;
-
-	if (n > 0)
-	{
-		for (i = 0; i < n; i++)
-		{
-			_putchar(95);
-		}
-		_putchar('\n');
-	}
-	else
-		_putchar('\n');
+int i;
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
+else
+_putchar('\n');
 }
