e8b17cc61c7e61890640b5f9ca85f45968783ded
root <alajaleenn1@gmail.com>
2025-09-04 15:33:00 +0000

    push

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,34 +2,28 @@
 #include "main.h"
 
 /**
- * fizz_buzz - prints the fizz buzz program
- *
- * My function
- *
- * Return: Always 0 (Success)
- */
-
-void fizz_buzz(void)
+* main - prints the numbers
+*
+* My function
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{	
+int i;
+for (i = 1; i <= 100; i++)
 {
-    int i;
-    for(i=1;i<=100;i++)
-    {
-        if(i%3==0 && i%5==0)
-        {
-            printf("FizzBuzz");
-        }
-        else if (i%3==0)
-        {
-            printf("Fizz");
-        }
-        else if (i%5==0)
-        {
-            printf("Buzz");
-        }
-        else
-        {
-            printf("%d",i);
-        }
-        printf("\n");
-    }
+if ((i % 3 == 0) & (i % 5 == 0))
+printf("FizzBuzz");
+else if (i % 3 == 0)
+printf("Fizz");
+else if (i % 5 == 0)
+printf("Buzz");
+else
+printf("%d", i);
+if (i < 100)
+printf(" ");
+}
+printf("\n");
+return (0);
 }
