41df7746995dcf6dd69cf7aa1ed771255d12153e
Najla Alajaleen <alajaleenn1@gmail.com>
2025-09-04 18:19:56 +0300

    Add 9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * fizz_buzz - prints the fizz buzz program
+ *
+ * My function
+ *
+ * Return: Always 0 (Success)
+ */
+
+void fizz_buzz(void)
+{
+    int i;
+    for(i=1;i<=100;i++)
+    {
+        if(i%3==0 && i%5==0)
+        {
+            printf("FizzBuzz");
+        }
+        else if (i%3==0)
+        {
+            printf("Fizz");
+        }
+        else if (i%5==0)
+        {
+            printf("Buzz");
+        }
+        else
+        {
+            printf("%d",i);
+        }
+        printf("\n");
+    }
+}
