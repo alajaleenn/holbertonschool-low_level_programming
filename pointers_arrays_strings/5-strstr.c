669a5d2dd6187a0497b37a4968010dfcd366c592
root <alajaleenn1@gmail.com>
2025-09-11 14:33:56 +0000

    push

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,26 @@
-#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * compare - compare the char
+ *
+ * @x: pointer to a char
+ * @y: pointer to a char
+ *
+ * Return: y
+ */
+
+int compare(char *x, char *y)
+{
+	while (*x && *y)
+	{
+		if (*x != *y)
+			return (0);
+		x++;
+		y++;
+	}
+	return (*y == '\0');
+}
 
 /**
  * _strstr - locates a substring
